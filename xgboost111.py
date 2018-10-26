ca661cd7aec7234c747378dba4a8bcb06e1fc798
YangGeYangGe <171951502@qq.com>
2018-10-26 14:55:10 +1100

    cross validation updated in python file

--- xgboost111.py
+++ xgboost111.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import itertools
 from sklearn.metrics import precision_recall_fscore_support
-
+    
 train_data, train_label = load_mnist("","train")
 test_data,test_label = load_mnist("","t10k")
 
@@ -26,6 +26,37 @@ X_main_std = sc.fit(train_data)
 train_data = X_main_std.transform(train_data)
 test_data = X_main_std.transform(test_data)
 
+def cv_test(max_depth = 2):
+    kf = KFold(n_splits = 10)
+    accs = []
+    # all_train = xgb.DMatrix(train_data, label=train_label)
+    time1 = time.time()
+    i = 0
+    for train_index, val_index in kf.split(train_data):
+        x_train = train_data[train_index]
+        y_train = train_label[train_index]
+        x_valid = train_data[val_index]
+        y_valid = train_label[val_index]
+        dtrain = xgb.DMatrix(x_train, label=y_train)
+        dval = xgb.DMatrix(x_valid, label=y_valid)
+        evallist = [(dtrain, 'train'),(dval, 'validation')]
+        #training
+        early_stopping = 5
+        bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=early_stopping,verbose_eval=False)
+        #predicting
+        pred = bst.predict(dval)
+        acc_rate = np.sum(pred == y_valid) / y_valid.shape[0]
+        bst.__del__()
+        accs.append(acc_rate)
+        print(i)
+        i += 1
+    time2 = time.time()
+    total_time = time2-time1
+    print(total_time)
+    print(sum(accs)/10)
+
+
+
 param = {}
 param['tree_method'] = 'approx'
 # param['tree_method'] = 'exact'
@@ -36,65 +67,30 @@ param["subsample"] = 0.8
 param["colsample_bytree"] = 0.8
 param["eval_metric"] = 'merror'
 param['silent'] = 1
-num_round = 100
+num_round = 5
 
-# [1,3,5,7,9]
-param['max_depth'] = 5
+# [2,4,6,10,15]
+depth = 1
+param['max_depth'] = depth
 
-import time
-kf = KFold(n_splits = 10)
 dtest = xgb.DMatrix(test_data, label=test_label)
-accs = []
-precisions = np.zeros((10,))
-recalls = np.zeros((10,))
-f1s = np.zeros((10,))
 
-time1 = time.time()
-i = 0
-for train_index, val_index in kf.split(train_data):
-    x_train = train_data[train_index]
-    y_train = train_label[train_index]
-    x_valid = train_data[val_index]
-    y_valid = train_label[val_index]
-    dtrain = xgb.DMatrix(x_train, label=y_train)
-    dval = xgb.DMatrix(x_valid, label=y_valid)
-    evallist = [(dtrain, 'train'),(dval, 'validation')]
-    
-    #training
-    early_stopping = 5
-    
-    bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=early_stopping,verbose_eval=False)
-    
-    #predicting
-    pred = bst.predict(dtest)
-    acc_rate = np.sum(pred == test_label) / test_label.shape[0]
-    
-    bst.__del__()
-    
-    accs.append(acc_rate)
-    pr, re, f1, size = precision_recall_fscore_support(test_label, pred)
-    
-#     precisions.append(pr)
-#     recalls.append(re)
-#     f1s.append(f1)
-    precisions += pr
-    recalls += re
-    f1s += f1
+cv_test(depth)
 
-    print(i)
-    i += 1
-#     print(pr.shape)
 
-time2 = time.time()
-total_time = time2-time1
+# x_train, x_valid, y_train, y_valid = train_test_split(train_data, train_label, test_size=0.2)
+# dtrain = xgb.DMatrix(x_train, label=y_train)
+# dval = xgb.DMatrix(x_valid, label=y_valid)
 
-precisions /= 10
-recalls /= 10
-f1s /= 10
+# evallist = [(dtrain, 'train'),(dval, 'validation')]
 
-print(total_time)
-print(sum(accs)/10)
-print(precisions)
-print(recalls)
-print(f1s)
+# early_stopping = 5
+# time1 = time.time()
+# bst = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=early_stopping)
+# pred = bst.predict(dtest)
+# time2 = time.time()
 
+# acc_rate = np.sum(pred == test_label) / test_label.shape[0]
+# print('Test accuracy using softmax = {}'.format(acc_rate))
+# runtime = time2-time1
+# print(runtime," seconds")
